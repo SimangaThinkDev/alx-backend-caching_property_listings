92c2e8985a5a57c38db8165dc59050183dcf9de8
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-08-30 22:03:52 +0200

    Done creating django app and added schema to it

---
+++ properties/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+# Create your models here.
+class Property(models.Model):
+    """Schema base for the Property table in the Database"""
+
+    # title(CharField, max_length=200)
+    # description(TextField)
+    # price (DecimalField, maxdigits=10, decimalplaces=2)
+    # location (CharField, max_length=100)
+    # created_at (DateTimeField, autonowadd=True)
+
+    title = models.CharField( max_length=200 )
+    description = models.TextField()
+    price = models.DecimalField( max_digits=10, decimal_places=2 )
+    location = models.CharField( max_length=100 )
+    created_at = models.DateTimeField( auto_now_add=True )
+
+    def __str__(self):
+        return self.title
+    
+    
